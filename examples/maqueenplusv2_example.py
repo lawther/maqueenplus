d5c7a1349c96f4457e4bd4441f9199f6901a5537
lawther <mike.lawther@gmail.com>
2024-08-12 11:41:55 +1000

    Add example for V2

---
+++ examples/maqueenplusv2_example.py
@@ -0,0 +1,13 @@
+from micropython import const
+import maqueenplusv2
+from time import sleep_ms
+
+_MOTOR_SPEED = const(50)
+
+mq = maqueenplusv2.MaqueenPlusV2()
+
+mq.set_underglow(mq.COLOR_GREEN)
+mq.motor_run(mq.MOTOR_BOTH, mq.MOTOR_DIR_FORWARD, _MOTOR_SPEED)
+sleep_ms(1000)
+mq.set_underglow(mq.COLOR_RED)
+mq.motor_stop(mq.MOTOR_BOTH)
