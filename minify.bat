11889451ee791e9fb54f3896e77ad7e14c6143c1
lawther <mike.lawther@gmail.com>
2024-08-16 10:55:25 +1000

    Updated batch file to use the renaming

--- minify.bat
+++ minify.bat
@@ -1,16 +1,19 @@
 @echo off
 setlocal
 
-call :minify maqueenplus.py
-call :minify maqueenplusv2.py
-call :minify huskylens.py
+call :minify maqueenplus
+call :minify maqueenplusv2
+call :minify huskylens
 echo Minification complete
 goto :EOF
 
 :minify
 :: Minify a file and report befoe and after size
-pyminify %1 --remove-debug --remove-literal-statements --output .\minified\%1
-for %%A in (%1) do set "orig_filesize=%%~zA"
-for %%A in (.\minified\%1) do set "minified_filesize=%%~zA"
-echo Filesize of %1 reduced from %orig_filesize% bytes to %minified_filesize% bytes
+set filename=%1.py
+python minimal_renamer.py %filename%
+pyminify %1-min.py --remove-debug --remove-literal-statements --output .\minified\%filename%
+for %%A in (%filename%) do set "orig_filesize=%%~zA"
+for %%A in (.\minified\%filename%) do set "minified_filesize=%%~zA"
+echo Filesize of %filename% reduced from %orig_filesize% bytes to %minified_filesize% bytes
+del %1-min.py
 goto :EOF
